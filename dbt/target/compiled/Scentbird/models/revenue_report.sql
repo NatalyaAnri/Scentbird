b72cb5457b6be7565d7762592d8ce19c5aea23ca
NatalyaAnri <anri.natalya@gmail.com>
2024-03-06 19:11:23 +0200

    del target/* from repo

--- dbt/target/compiled/Scentbird/models/revenue_report.sql
+++
@@ -1,6 +0,0 @@
-
-
-select date_trunc('month', created_at)::date as report_date
-     , sum(amount)                           as total_amount
-from "postgres"."public_exercises"."orders"
-group by report_date
